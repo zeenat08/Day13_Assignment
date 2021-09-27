84e3b10b81fe2277b655169700179363ff20e7b2
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-27 22:59:01 +0530

    ZEENAT: added maximum string number check

---
+++ UC3_Maximum_String_Num_Check.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Assignment
+{
+    class UC3_Maximum_String_Num_Check
+    {
+        public static string MaximumStringNumber(string firstString, string secondString, string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+               firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+               firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+                return firstString;
+            }
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+               secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+               secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                return secondString;
+            }
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+               thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+               thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+            throw new Exception("FirstNumber, SecondNumber and ThirdNumber are same or Maybe 2 Numbers are same");
+        }
+    }
+}
+
+
+        
+
+    
