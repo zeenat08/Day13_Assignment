94c924a324f53f8a13255a253242bf8a9c9cb1ad
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-27 22:58:22 +0530

    ZEENAT: added maximum float number check

---
+++ UC2_Maximum_Float__Num_Check.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Assignment
+{
+    class UC2_Maximum_Float__Num_Check
+    {
+        public static float MaxFloatNumber(float firstNum, float secondNum, float thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+            {
+                return firstNum;
+            }
+            if (secondNum.CompareTo(thirdNum) > 0 && secondNum.CompareTo(firstNum) > 0)
+            {
+                return secondNum;
+            }
+            if (thirdNum.CompareTo(firstNum) > 0 && thirdNum.CompareTo(secondNum) > 0)
+            {
+                return thirdNum;
+            }
+            throw new Exception("FirstNumber, SecondNumber and ThirdNumber are same or Maybe 2 Numbers are same");
+        }
+    }
+}
