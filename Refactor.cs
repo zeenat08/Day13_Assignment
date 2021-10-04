c52fc4acbf79a5aa90cb5626db3d67986ec9d8a3
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-05 00:34:29 +0530

    ZEENAT:[Added] Refactoring all the 3 methods and finding max value

---
+++ Refactor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Assignment
+{
+    public class Refactor_Generic_Method
+        {
+            public T findMaximum<T>(T first, T second, T third) where T : struct, IComparable<T>
+            {
+                if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+                {
+                    Console.WriteLine("The Maximum data is : " + first);
+                    return first;
+                }
+                else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+                {
+                    Console.WriteLine("The Maximum data is : " + second);
+                    return second;
+                }
+                else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+                {
+                    Console.WriteLine("The Maximum data is : " + third);
+                    return third;
+                }
+                return default;
+            }
+        }
+    }
+
+
