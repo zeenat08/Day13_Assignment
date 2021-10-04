7c8746ccd984d857440cb2aec85c69bb0a1ab848
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-05 00:27:28 +0530

    ZEENAT:[Added] Extending The Max Method and Finding Maximum Value using array

---
+++ UC4_Sorting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Assignment
+{
+    class UC4_Sorting<T> where T : IComparable
+    {
+        public T[] value;
+        public UC4_Sorting(T[] value)
+        {
+            this.value = value;
+        }
+
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[sorted_values.Length - 1];
+        }
+    }
+}
+
