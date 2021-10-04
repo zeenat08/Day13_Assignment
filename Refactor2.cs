51a75611aa5b061ddb19462c12f984e54948b267
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-05 00:36:15 +0530

    ZEENAT:[Added] Refactoring all the 3 methods and finding max value in a generic class

---
+++ Refactor2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Assignment
+{
+    public class Refactor2_Generic_Class<T> where T : IComparable
+    {
+        private T first, second, third;
+        public Refactor2_Generic_Class(T first, T second, T third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+        public void findMaximum()
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("The Greatest is : " + first);
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("The Greatest  is : " + second);
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine("The Greatest  is : " + third);
+            }
+            else
+                Console.WriteLine("All are equal");
+        }
+    }
+}
