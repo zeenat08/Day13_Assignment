592a0cd847ca392c6b3f0bbc2ff6108a777d2d50
zeenat08 <zeenatkhanbfs@gmail.com>
2021-10-05 00:29:29 +0530

    ZEENAT:[Added] Extending The Max Method to Print Max Value

---
+++ UC5_Extending_Method.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_Assignment
+{
+    class UC5_Extending_Method<T> where T : IComparable
+    {
+        public T[] value;
+        public UC5_Extending_Method(T[] value)
+        {
+            this.value = value;
+        }
+
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+
+        }
+
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[sorted_values.Length - 1];
+        }
+
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
+        }
+
+    }
+}
+    
+
